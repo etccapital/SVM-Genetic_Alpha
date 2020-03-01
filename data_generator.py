0dc8a575856a41ecc391fb97a11489c8b560d7b0
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-03-01 13:36:39 -0500

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,13 +1,3 @@
-#
-# Copyright (c) 2020. Asutosh Nayak (nayak.asutosh@ymail.com)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-
 import os
 import re
 from operator import itemgetter
